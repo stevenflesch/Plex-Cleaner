fe8050b8337ef7c6c21d9a1823578db5f1871622
Steven Flesch <steve@flywithsteve.com>
2017-04-16 16:29:48 -0700

    Add option to send email of log contents at completion of script using yagmail.

--- PlexCleaner.py
+++ PlexCleaner.py
@@ -25,6 +25,10 @@ IgnoreSections = []  # Sections to skip cleaning, for use when Settings['Section
 LogFile = ""  # Location of log file to save console output
 LogFileMode = "overwrite"  # File Mode for logging, overwrite or append, default is overwrite
 trigger_rescan = False  # trigger_rescan will rescan a section if changes are made to it
+EmailLog = False  # Email the log file contents at conclusion of script
+EmailUsername = ""  # Email server username
+EmailPassword = ""  # Email server password - if using Gmail, you can use an "app password" so your regular email password isn't in plain text in your config. See: https://myaccount.google.com/apppasswords
+EmailRecipient = ""  # Email address to receive the log file contents, if enabled.
 
 # Use Username/Password or Token for servers with PlexHome
 # To generate a proper Token, first put your username and password and run the script with the flag --test.
@@ -281,6 +285,10 @@ def LoadSettings(opts):
     s['LogFile'] = opts.get('LogFile', LogFile)
     s['LogFileMode'] = opts.get('LogFileMode', LogFileMode)
     s['trigger_rescan'] = opts.get('trigger_rescan', trigger_rescan)
+    s['EmailLog'] = opts.get('EmailLog', EmailLog)
+    s['EmailUsername'] = opts.get('EmailUsername', EmailUsername)
+    s['EmailPassword'] = opts.get('EmailPassword', EmailPassword)
+    s['EmailRecipient'] = opts.get('EmailRecipient', EmailRecipient)
     s['Token'] = opts.get('Token', Token)
     s['Username'] = opts.get('Username', Username)
     s['Password'] = opts.get('Password', Password)
@@ -1086,3 +1094,37 @@ if len(ActionHistory) > 0:
 log("")
 log("----------------------------------------------------------------------------")
 log("----------------------------------------------------------------------------")
+
+# Email Log
+if Settings['EmailLog']:
+    try:
+        import yagmail
+        EmailContents = [] # Text of email.
+        EmailContents.append("----------------------------------------------------------------------------")
+        EmailContents.append("                Summary -- Script Completed Successfully")
+        EmailContents.append("----------------------------------------------------------------------------")
+        EmailContents.append("\n")
+        EmailContents.append("  Total File Count      " + str(FileCount))
+        EmailContents.append("  Kept Show Files       " + str(KeptCount))
+        EmailContents.append("  On Deck Files         " + str(OnDeckCount))
+        EmailContents.append("  Deleted Files         " + str(DeleteCount))
+        EmailContents.append("  Moved Files           " + str(MoveCount))
+        EmailContents.append("  Copied Files          " + str(CopyCount))
+        EmailContents.append("  Flagged Files         " + str(FlaggedCount))
+        EmailContents.append("  Rescanned Sections    " + ', '.join(str(x) for x in RescannedSections))
+        if len(ActionHistory) > 0:
+            EmailContents.append("\n")
+            EmailContents.append("  Changed Files:")
+            for item in ActionHistory:
+                EmailContents.append("  " + str(item))
+        EmailContents.append("\n")
+        EmailContents.append("----------------------------------------------------------------------------")
+        try:
+            yag = yagmail.SMTP(Settings['EmailUsername'], Settings['EmailPassword'])
+            yag.send(Settings['EmailRecipient'], 'Plex-Cleaner.py Log', EmailContents)
+            log("")
+            log("Email of log file contents sent successfully.")
+        except:
+            log("Could not send email.  Please ensure a username, password, and recipient are specified in your Config file.")
+    except:
+        log("Unable to send log via email.  Please install yagmail using the command 'pip install yagmail[all]' ...")
